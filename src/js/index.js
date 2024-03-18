42e5e468d243f1c82fa8634f48cd7dc53bb72671
Uarles <121899351+Uarles@users.noreply.github.com>
2024-03-18 20:11:41 -0300

    13 commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector(".icone-html");
+
+const elementoHtml = document.querySelector(".icone-html .text-html");
+
+botao.addEventListener ("click", () => {
+    
+     elementoHtml.classList.toggle("ativo");
+
+}); 
