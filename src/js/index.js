8fec26511e23d9b25ee7b3d3c49e5d54ba15ed14
Uarles <121899351+Uarles@users.noreply.github.com>
2024-03-29 08:47:10 -0300

    15 commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,83 @@ botao.addEventListener ("click", () => {
      elementoHtml.classList.toggle("ativo");
 
 }); 
+
+const botaocss = document.querySelector(".icone-css");
+
+const elementocss = document.querySelector(".icone-css .text-css");
+
+botaocss.addEventListener ("click", () => {
+    
+     elementocss.classList.toggle("ativo");
+
+}); 
+
+const botaoJs = document.querySelector(".icone-javascript");
+
+const elementoJs = document.querySelector(".icone-javascript .text-javascript");
+
+botaoJs.addEventListener ("click", () => {
+    
+     elementoJs.classList.toggle("ativo");
+
+}); 
+
+const botaoJava = document.querySelector(".icone-java");
+
+const elementoJava = document.querySelector(".icone-java .text-java");
+
+botaoJava.addEventListener ("click", () => {
+    
+     elementoJava.classList.toggle("ativo");
+
+}); 
+
+const botaoSql = document.querySelector(".icone-sql");
+
+const elementoSql = document.querySelector(".icone-sql .text-sql");
+
+botaoSql.addEventListener ("click", () => {
+    
+     elementoSql.classList.toggle("ativo");
+
+}); 
+
+const botaoMysql = document.querySelector(".icone-mysql");
+
+const elementoMysql = document.querySelector(".icone-mysql .text-mysql");
+
+botaoMysql.addEventListener ("click", () => {
+    
+     elementoMysql.classList.toggle("ativo");
+
+}); 
+
+const botaoPhp = document.querySelector(".icone-php");
+
+const elementoPhp = document.querySelector(".icone-php .text-php");
+
+botaoPhp.addEventListener ("click", () => {
+    
+     elementoPhp.classList.toggle("ativo");
+
+}); 
+
+const botaoReact = document.querySelector(".icone-react");
+
+const elementoReact = document.querySelector(".icone-react .text-react");
+
+botaoReact.addEventListener ("click", () => {
+    
+     elementoReact.classList.toggle("ativo");
+
+}); 
+
+const botaoComple = document.querySelector(".icone-complementar");
+
+const elementoComple = document.querySelector(".icone-complementar .text-complementar");
+
+botaoComple.addEventListener ("click", () => {
+    
+     elementoComple.classList.toggle("ativo");
+
+}); 
